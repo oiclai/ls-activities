b368520f95639c57dcc17e55158434222d4509e5
oiclai <claradealcantara285@gmail.com>
2024-11-21 00:37:59 -0300

    :bug: fix: fixing mistakes

--- projetos-avaliativos/objetoExames.js
+++ projetos-avaliativos/objetoExames.js
@@ -1,31 +1,12 @@
 // lembrete p mim: callbacks são muito usados jem funcoes de array como map(), filter(), reduce()*
 class Exam {
     constructor(answer, weight) {
-        this.answer = answer;
-        this.weight = weight; 
-        this.exams = []; 
+        this.answer = answer; // resposta correta
+        this.weight = weight; // peso das questoes
+        this.exams = []; // respostas do aluno q ainda serao adcionadas
     }
-
-    add(examDaVez) {
-        this.exams.push(examDaVez); 
-    }
-
-    avg() {
-        const calculateGrade = (exams, answer) => {
-            let totalGrade = 0;
-            for (const exam of exams) {
-                for (let i = 0; i < answer.length; i++) {
-                    if (exam.responses[i] === answer[i]) {
-                        totalGrade += exam.weight[i];
-                    }
-                }
-            }
-
-            return totalGrade / exams.length;
-        };
-
-        return calculateGrade(this.exams, this.answer);
+    add(respostasAluno) {
+        this.exams.push(respostasAluno); 
     }
 }
-
-    
+// n to entendendo oq é p fazer, perguntar ao professor
