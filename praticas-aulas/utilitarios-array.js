3540f60a2a632cec48be1914e110128755846451
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-11-24 17:26:41 -0300

    :books: docs: add files

---
+++ praticas-aulas/utilitarios-array.js
@@ -0,0 +1,40 @@
+function min (array){
+    let menor = Infinity; // teste
+    for (const i of array) {
+      if (i < menor){
+        menor = i;
+      }
+    }
+    return menor;
+  }
+  
+function max (array){
+    let maior = -(Infinity); // teste
+    for (const i of array) {
+      if (i > maior){
+        maior = i;
+      }
+    }
+    return maior;
+  }
+  
+function range (num){
+    array = []
+    for (let flag = 0; flag <= num; flag +=1){
+      array.push(flag);
+    }
+  }
+  
+function range (numInicial, numFinal){
+    array = []
+    for (let flag = numInicial; flag <= numFinal; flag +=1){
+      array.push(flag);
+    }
+  }
+  
+function range (numInicial, numFinal, numPassos){
+    array = []
+    for (let flag = numInicial; flag <= numFinal; flag +=numPassos){
+      array.push(flag);
+    }
+  }
