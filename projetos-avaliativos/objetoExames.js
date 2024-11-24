972a11765b094cbd0ed9fbadb49b58761e83b94e
oiclai <claradealcantara285@gmail.com>
2024-11-24 01:43:51 -0300

    :sparkles: feat: add features (methods)

--- projetos-avaliativos/objetoExames.js
+++ projetos-avaliativos/objetoExames.js
@@ -7,12 +7,33 @@ class Exam {
     }
     add(exam) {
         let nota = 0;
-            for (const questao in this.answer.alternativas) {
-                if (exam.alternativas[questao] === this.answer.alternativas[questao]) {
-                    nota += this.weight[questao];
-                }
+        for (const questao in this.answer.alternativas) {
+            if (exam.alternativas[questao] === this.answer.alternativas[questao]) {
+                nota += this.weight[questao];
             }
+        }
         exam.nota = nota;
         this.exams.push(exam); // conclui adicionando a nota do aluno no array
     }
+    avg(){
+        let somaNotas = 0;
+        for(let exam of this.exams){ 
+        somaNotas += exam.nota;
+        }
+        let media = somaNotas/this.exams.length; 
+        media = media.toFixed(1); // 100% opicional mas acho q fica melhor assim
+        return media;
+  }
+
+  min(){
+        let notas = this.exams.map(exam => exam.nota); 
+        let menorNota = Math.min(...notas); // Math.min para encontrar o menor valor no caso a nota
+        return [menorNota]; // mas retorna como um array
+  }
+
+  max(){
+    let notas = this.exams.map(exam => exam.nota); 
+    let maiorNota = Math.max(...notas); // Math.max para encontrar o maior valor no caso a nota
+    return [maiorNota];
+}
 }
