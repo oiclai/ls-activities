313a178c3c23781a86f34426e1386e8404d593b1
oiclai <claradealcantara285@gmail.com>
2024-11-24 01:24:30 -0300

    :construction: in progress ...

--- projetos-avaliativos/objetoExames.js
+++ projetos-avaliativos/objetoExames.js
@@ -1,12 +1,18 @@
 // lembrete p mim: callbacks são muito usados jem funcoes de array como map(), filter(), reduce()*
 class Exam {
     constructor(answer, weight) {
-        this.answer = answer; // resposta correta
+        this.answer = answer; // gabarito
         this.weight = weight; // peso das questoes
-        this.exams = []; // respostas do aluno q ainda serao adcionadas
+        this.exams = []; // exames feitos
     }
-    add(respostasAluno) {
-        this.exams.push(respostasAluno); 
+    add(exam) {
+        let nota = 0;
+            for (const questao in this.answer.alternativas) {
+                if (exam.alternativas[questao] === this.answer.alternativas[questao]) {
+                    nota += this.weight[questao];
+                }
+            }
+        exam.nota = nota;
+        this.exams.push(exam); // conclui adicionando a nota do aluno no array
     }
 }
-// n to entendendo oq é p fazer, perguntar ao professor
