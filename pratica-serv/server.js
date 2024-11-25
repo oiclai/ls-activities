f637e0279020cdb25d2413984c598f853b6222b6
oiclai <claradealcantara285@gmail.com>
2024-11-25 00:46:16 -0300

    :construction: possibly still in progress

--- pratica-serv/server.js
+++ pratica-serv/server.js
@@ -1,16 +1,17 @@
-// node pratica-serv/server.js
-import express from 'express';
+import express from "express";
 
 const app = express(); // server = app
+app.listen(3000, () => {
+    console.log("Servidor escutando..."); // 3000 = porta padrao server local
+});
+// node pratica-serv/server.js
 
-app.listen(3000), () => {
-    console.log('Servidor escutando . . . ');
-}; // 3000 = porta padrao server local
-
+app.get("/api", (req, res) => {
+    res.status(200).send("Testeeeeeeeeeeeeeeeeeeeeeeeee");
+});
 
 // rota do cliente para pegar uma resposta do servidor
 // requisicao e resposta
 // 'Ctrl C' no terminal -> derruba o servidor 
-app.get('/api', (req, res) => {
-    res.status(200).send('Manda Mensagem . . . ');
-});
+
+
