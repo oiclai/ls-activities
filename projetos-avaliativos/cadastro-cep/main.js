3197b8acd96ef6fbccaadaf1cd94590148011135
oiclai <claradealcantara285@gmail.com>
2025-02-11 21:27:45 -0300

    :bulb: docs: comentarios

--- projetos-avaliativos/cadastro-cep/main.js
+++ projetos-avaliativos/cadastro-cep/main.js
@@ -1,36 +1,37 @@
+/*
+na tentativa de tratar o evento usando o tecla 'Enter' 
+-> levando para a funcao 'buscarEndereco()' onde a API será chamada
+*/
 document.addEventListener("DOMContentLoaded", () => {
     const cepInput = document.getElementById("cep");
 
-    // Detecta quando o usuário pressiona "Enter"
+    // user aperta ENTER para o preenchimento automatico
     cepInput.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            event.preventDefault(); // Evita o envio do formulário
+            event.preventDefault(); // Evita o envio do form
             buscarEndereco();
         }
     });
 });
-
 function buscarEndereco() {
-    const cep = document.getElementById('cep').value.replace('-', '').trim(); // Remove o hífen e espaços extras
     if (cep.length !== 8) {
         alert("Por favor, insira um CEP válido com 8 dígitos.");
         return;
     }
+    const url = `https://viacep.com.br/ws/${cep}/json/`; // link da API
 
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-
-// -------------------- req -------------------------------
+// ---------------------------- req -------------------------------
     fetch(url)
-        .then(response => response.json())
+        .then(response => response.json()) // promisse
         .then(data => {
             if (data.erro) {
                 alert("CEP não encontrado. Por favor, verifique o CEP informado.");
                 return;
             }
 
-            // Preenchendo os campos do formulário com os dados recebidos
+            // preenchendo os Campos com os dados da APiiiiiiiiiiiiiiiii
             document.getElementById('rua').value = data.logradouro || '';
-            document.getElementById('numero').value = ''; // Campo de número não preenchido pela API
+            document.getElementById('numero').value = ''; //  preenchido pel0 user
             document.getElementById('bairro').value = data.bairro || '';
             document.getElementById('cidade').value = data.localidade || '';
             document.getElementById('estado').value = data.uf || '';
